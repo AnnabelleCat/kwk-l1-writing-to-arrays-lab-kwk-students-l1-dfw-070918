47b1ed27887fdf9dd5fdb01425d804213e5664b7
AnnabelleCat <pikainka@gmail.com>
2018-07-10 20:43:07 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,6 +4,8 @@ rainbow_colors = [
   "light_cyan"
 ]
 
+puts rainbow_colors[0]
+
 def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
   rainbow_colors[0] = "red"
